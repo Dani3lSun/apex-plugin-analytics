4448af03d5d9f451541e0a77b56aaf465be1c295
Daniel Hochleitner <dhochleitner@posteo.de>
2018-12-28 16:41:51 +0100

    reorganized folder structure of app db objects

--- app/db/install.sql
+++ app/db/install.sql
@@ -1,23 +1,23 @@
 SPOOL install.log
 
 -- create sequences
-@analytics_data_seq.sql
-@analytics_data_geolocation_seq.sql
-@apex_app_pages_seq.sql
-@custom_analytic_queries_seq.sql
+@@sequences/analytics_data_seq.sql
+@@sequences/analytics_data_geolocation_seq.sql
+@@sequences/apex_app_pages_seq.sql
+@@sequences/custom_analytic_queries_seq.sql
 
 -- create tables
-@analytics_data_table.sql
-@analytics_data_geolocation_table.sql
-@apex_apps_table.sql
-@apex_app_pages_table.sql
-@custom_analytic_queries_table.sql
+@@tables/analytics_data_table.sql
+@@tables/analytics_data_geolocation_table.sql
+@@tables/apex_apps_table.sql
+@@tables/apex_app_pages_table.sql
+@@tables/custom_analytic_queries_table.sql
 
 -- create packages
-@apexanalytics_app_pkg.pks
-@apexanalytics_app_pkg.pkb
+@@packages/apexanalytics_app_pkg.pks
+@@packages/apexanalytics_app_pkg.pkb
 
 -- create jobs
-@create_geolocation_scheduler_job.sql
+@@jobs/create_geolocation_scheduler_job.sql
 
 SPOOL OFF
